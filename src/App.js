95883534737ac49ab2498b9773f54cab7a5b5a0c
Murtuza Chakkiwala <murtuza.chakkiwala@CIGNEXDATAMATICS.COM>
2019-10-16 18:26:27 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Dashboard from "./pages/dashboard";
 function App() {
   return (
     <div className="App">
-    <Router basename={'/liferay-react-demo'}>
+    <Router basename={'/'}>
       <Route
         exact
         path="/"
